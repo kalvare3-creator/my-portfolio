8eb8182de8aaaa3f2e706310d7eea79aac83a122
Katherine <umanzorgabriela15u@gmail.com>
2026-08-14 01:10:24 -0400

    Update portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,11 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
-import About from "@/components/About";
-import Projects from "@/components/Projects";
-import Experience from "@/components/Experience";
-import Footer from "@/components/Footer";
-
-
 
 export default function Home() {
   return (
     <>
       <Navbar />
-
-      <main>
-        <Hero />
-       
-      </main>
-
-    
+      <Hero />
     </>
   );
 }
